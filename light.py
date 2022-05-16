5222e0e0a530b7cf41aff7837d8bbb798ab387a9
ray <ray.dykeman@gmail.com>
2022-05-16 12:55:01 -0400

    Moved rayLensIntersection to main Combining refraction into single module used for each lens

--- light.py
+++ light.py
@@ -41,40 +41,16 @@ class Light():
         self.ray.append([x, y])
 
 
-    """
-    def rayLensIntersection(self, lens):
-        #intersection = []
-        #print(f"ray = {ray[-1]}")
-        y = self.ray[-1][1]
-        #print(f"radius, y, self.y_c = {self.radius, y, self.y_c}")
-        #print(f"asin(inside) = {(y + self.y_c) / self.radius}")
-        #if (y + self.y_c) / self.radius > 1:
-            #theta = math.pi / 4
-        #else:
-        print(f"y = {y}, self.y_c = {lens.y_c}")
-        print(f"asin = {(y + lens.y_c) / lens.radius}")
-        theta = math.pi - math.asin((y + lens.y_c) / lens.radius)
-        print(f"theta = {theta * 180 / math.pi} radius = {lens.radius}")
-
-        x = lens.x_c + lens.radius * math.cos(theta)
-        y = lens.y_c + lens.radius * math.sin(theta)
-        print(f"x, y = {x, y}")
-        self.ray.append([x, y])
-        #print(f"inside ray = {ray}")
-        return self.ray
-    """
-
-
 
     def refraction(self, lens, n1, n2):
         """Ray - Lens1 Interaction and refraction"""
-        lens.rayLensIntersection(self.ray, self.angle)
+        #lens.rayLensIntersection(self.ray, self.angle)
         # Compute Normal Vector
         unitNormalVector = lens.normalVect(self.ray)
         #if n1 == 1.5:
         #    print(f"unitNormalVector = {unitNormalVector}")
         # Compute ray Unit Normal Vector
-        print(f"33 ray = {self.ray}, angle = {self.angle}")
+        print(f"77 ray = {self.ray}, angle = {self.angle}")
         rayUnitVector = [math.cos(self.angle[-1]), math.sin(self.angle[-1])]
         #if self.ray[0][1] == 40:
         #print(f"rayUnitVector = {rayUnitVector}")
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ surface1 = lens1.equation()
 
 
 """ Create lens2 class object """
-lens2 = Lens(30, 965, 0, 1.5, 1.0)
+lens2 = Lens(30, 980, 0, 1.5, 1.0)
 surface2 = lens2.equation()
 
 
@@ -30,36 +30,42 @@ for i in range(numberLightRays): #
 """ LIGHT SOURCE POINTS IN LIGHT OBJECTS """
 for lightBeam in light:
     lightBeam.lightSource()
-    #print(f"33 ray = {lightBeam.ray}")
 
-""" LIGHT LENS INTERSECTION """
+
+""" LIGHT LENS1 INTERSECTION """
 for lightBeam in light:
     lightBeam.rayLensIntersection(lens1)
 
 
 for lightBeam in light:
-    """Calculate ray lens1 intersection."""
+    """LIGHT LENS1 REFRACTION."""
     pass
-    #lightBeam.refraction(lens1, n1 = 1.0, n2 = 1.5)
+    lightBeam.refraction(lens1, n1 = 1.0, n2 = 1.5)
     print("44")
     #if lightBeam.ray[0][1] == 40:
     #print(f"46 lightBeam.ray[0][1] = {lightBeam.ray[0][1]}")
     #print(f"lightBeam.angle[-1] = {lightBeam.angle[-1]}")
 
+
+""" LIGHT LENS2 INTERSECTION """
 for lightBeam in light:
-    pass
-    #lightBeam.rayExtension()
+    lightBeam.rayLensIntersection(lens2)
+
+
+
 
 
 print("\n****************************************************************\n")
 for lightBeam in light:
     """ ray lens2 intersection"""
     pass
-    #lightBeam.refraction(lens2, n1 = 1.5, n2 = 1.0)
+    lightBeam.refraction(lens2, n1 = 1.5, n2 = 1.0)
     print()
 
-#for lightBeam in light:
-    #lightBeam.refraction2()
+for lightBeam in light:
+    pass
+    lightBeam.rayExtension()
+
 
 #******************************************8
 toScreen = Display()
